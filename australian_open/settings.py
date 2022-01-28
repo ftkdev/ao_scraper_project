152177ef8c0858ad417b1e5c01c92fdf369383eb
faustomakoto <faustomakoto@gmail.com>
2022-01-28 11:31:47 -0300

    Project almost done. Need to code the iteration for years and allow use for other tournaments.

--- australian_open/settings.py
+++ australian_open/settings.py
@@ -1,3 +1,4 @@
+from australian_open.uagents import get_random_agent
 # Scrapy settings for australian_open project
 #
 # For simplicity, this file contains only settings considered important or
@@ -14,18 +15,19 @@ NEWSPIDER_MODULE = 'australian_open.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'australian_open (+http://www.yourdomain.com)'
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+USER_AGENT = get_random_agent()
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 2
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- australian_open/spiders/aoscraper.py
+++ australian_open/spiders/aoscraper.py
@@ -1,52 +1,115 @@
+import  australian_open.xplist as xp
 import scrapy
-from ..items import AustralianOpenItem
+from australian_open.items import AustralianOpenItem
 from scrapy.loader import ItemLoader
 
 class AoscraperSpider(scrapy.Spider):
     name = 'aoscraper'
-    allowed_domains = ['tennis.com/tournaments/sr-tournament-2565-australian-open/']
-    start_urls = ['http://tennis.com/tournaments/sr-tournament-2565-australian-open//']
+    # allowed_domains = ['tennis.com/tournaments/sr-tournament-2565-australian-open/']
+    start_urls = [
+            'https://www.tennis.com/tournaments/sr-tournament-2565-australian-open/library/sr-tournament-2567/2022/',
+            'https://www.tennis.com/tournaments/sr-tournament-2565-australian-open/library/sr-tournament-2567/2021/',
+            'https://www.tennis.com/tournaments/sr-tournament-2565-australian-open/library/sr-tournament-2567/2020/'
+            ]
 
     def parse(self, response):
-        game_details_list = response.xpath("//div[@class='rounded']/div[@class=\
-                'mb-3 recipe-card recipe-card-with-hover']")
-
+        game_details_list = response.xpath(xp.game_details_list_xpath)
+        # year_list = [2022, 2021, 2020]
+        # for y in year_list:
         for game in game_details_list:
             l = ItemLoader(item=AustralianOpenItem(), selector=game)
-            l.add_xpath('tournament_phase', "")
-            l.add_value('calendar_year',"")
+            l.add_xpath('tournament_phase', xp.tournament_phase_xpath)
+            l.add_value('calendar_year', '2022')
 
             # Access the detail page to scrape game information
-            game_href= response.xpath("").get()
+            game_href= game.xpath(xp.game_href_xpath).get()
             yield response.follow(url=game_href, meta={'item' : l.load_item()},
                     callback=self.parse_game, dont_filter=True)
 
-
-    # Scrape the ingredients from the detail page
+    # Scrape the stats from the detail page
     def parse_game(self, response):
         l = ItemLoader(item=response.meta['item'], selector=response)
 
         #Parse player1 data
-        l.add_xpath('player1_name',"xpathss")
-        l.add_xpath('player1_surname',"xpathss")
-        l.add_xpath('player1_country',"xpathss")
-        l.add_xpath('player1_seed',"xpathss")
-        l.add_xpath('player1_1st_set',"xpathss")
-        l.add_xpath('player1_2st_set',"xpathss")
-        l.add_xpath('player1_3rd_set',"xpathss")
-        l.add_xpath('player1_4th_set',"xpathss")
-        l.add_xpath('player1_5th_set',"xpathss")
-        l.add_xpath('p1_total_points_won',"xpathss")
-        l.add_xpath('p1_total_games_won',"xpathss")
-        l.add_xpath('p1_max_points_row',"xpathss")
-        l.add_xpath('p1_max_games_row',"xpathss")
-        l.add_xpath('p1_tiebreaks_won',"xpathss")
-        l.add_xpath('p1_servgames_won',"xpathss")
-        l.add_xpath('p1_aces',"xpathss")
-        l.add_xpath('p1_doublefaults',"xpathss")
-        l.add_xpath('p1_1stserve_success',"xpathss")
-        l.add_xpath('p1_1stserve_pwon',"xpathss")
-        l.add_xpath('p1_2ndserve_pwon',"xpathss")
-        l.add_xpath('p1_breakpoints_won',"xpathss")
+        l.add_xpath('player1_name', xp.player1_name_xpath)
+        l.add_xpath('player1_surname', xp.player1_surname_xpath)
+        l.add_xpath('player1_country', xp.player1_country_xpath)
+        l.add_xpath('player1_seed', xp.player1_seed_xpath)
+        l.add_xpath('player1_total_sets', xp.player1_total_sets_xpath)
+        l.add_xpath('p1_total_points_won', xp.p1_total_points_won_xpath)
+        l.add_xpath('p1_total_games_won', xp.p1_total_games_won_xpath)
+        l.add_xpath('p1_max_points_row', xp.p1_max_points_row_xpath)
+        l.add_xpath('p1_max_games_row', xp.p1_max_games_row_xpath)
+        l.add_xpath('p1_tiebreaks_won', xp.p1_tiebreaks_won_xpath)
+        l.add_xpath('p1_servgames_won', xp.p1_servgames_won_xpath)
+        l.add_xpath('p1_aces', xp.p1_aces_xpath)
+        l.add_xpath('p1_doublefaults', xp.p1_doublefaults_xpath)
+        l.add_xpath('p1_1stserve_success', xp.p1_1stserve_success_xpath)
+        l.add_xpath('p1_1stserve_pwon', xp.p1_1stserve_pwon_xpath)
+        l.add_xpath('p1_2ndserve_pwon', xp.p1_2ndserve_pwon_xpath)
+        l.add_xpath('p1_breakpoints_won', xp.p1_breakpoints_won_xpath)
+
+        p1_sets_list = response.xpath(xp.p1_sets_list_xpath).getall()
+        try:
+            l.add_value('player1_1st_set', p1_sets_list[0])
+        except:
+            l.add_value('player1_1st_set', '0')
+        try:
+            l.add_value('player1_2nd_set', p1_sets_list[1])
+        except:
+            l.add_value('player1_2nd_set', '0')
+        try:
+            l.add_value('player1_3rd_set', p1_sets_list[2])
+        except:
+            l.add_value('player1_3rd_set', '0')
+        try:
+            l.add_value('player1_4th_set', p1_sets_list[3])
+        except:
+            l.add_value('player1_4th_set', '0')
+        try:
+            l.add_value('player1_5th_set', p1_sets_list[4])
+        except:
+            l.add_value('player1_5th_set', '0')
+
+        #Parse player2 data
+        l.add_xpath('player2_name', xp.player2_name_xpath)
+        l.add_xpath('player2_surname', xp.player2_surname_xpath)
+        l.add_xpath('player2_country', xp.player2_country_xpath)
+        l.add_xpath('player2_seed', xp.player2_seed_xpath)
+        l.add_xpath('player2_total_sets', xp.player2_total_sets_xpath)
+        l.add_xpath('p2_total_points_won', xp.p2_total_points_won_xpath)
+        l.add_xpath('p2_total_games_won', xp.p2_total_games_won_xpath)
+        l.add_xpath('p2_max_points_row', xp.p2_max_points_row_xpath)
+        l.add_xpath('p2_max_games_row', xp.p2_max_games_row_xpath)
+        l.add_xpath('p2_tiebreaks_won', xp.p2_tiebreaks_won_xpath)
+        l.add_xpath('p2_servgames_won', xp.p2_servgames_won_xpath)
+        l.add_xpath('p2_aces', xp.p2_aces_xpath)
+        l.add_xpath('p2_doublefaults', xp.p2_doublefaults_xpath)
+        l.add_xpath('p2_1stserve_success', xp.p2_1stserve_success_xpath)
+        l.add_xpath('p2_1stserve_pwon', xp.p2_1stserve_pwon_xpath)
+        l.add_xpath('p2_2ndserve_pwon', xp.p2_2ndserve_pwon_xpath)
+        l.add_xpath('p2_breakpoints_won', xp.p2_breakpoints_won_xpath)
+
+        p2_sets_list = response.xpath(xp.p2_sets_list_xpath).getall()
+        try:
+            l.add_value('player2_1st_set', p2_sets_list[0])
+        except:
+            l.add_value('player2_1st_set', '0')
+        try:
+            l.add_value('player2_2nd_set', p2_sets_list[1])
+        except:
+            l.add_value('player2_2nd_set', '0')
+        try:
+            l.add_value('player2_3rd_set', p2_sets_list[2])
+        except:
+            l.add_value('player2_3rd_set', '0')
+        try:
+            l.add_value('player2_4th_set', p2_sets_list[3])
+        except:
+            l.add_value('player2_4th_set', '0')
+        try:
+            l.add_value('player2_5th_set', p2_sets_list[4])
+        except:
+            l.add_value('player2_5th_set', '0')
         yield l.load_item()
 
